5b94b038794edf009459ac1ef241d1ed405962ad
stefanodvx <69367859+stefanodvx@users.noreply.github.com>
2023-05-21 12:04:02 +0200

    Update utils.py

--- pyanimeclick/utils.py
+++ pyanimeclick/utils.py
@@ -5,12 +5,11 @@ import re
 def find_matchin_tag(
     soup: BeautifulSoup,
     name: str,
-    pattern: str,
+    pattern: re.Pattern,
     **kwargs
 ):
-    compiled_pattern = re.compile(pattern)
-    tag = soup.find(name, **kwargs, string=compiled_pattern)
+    tag = soup.find(name, **kwargs, string=pattern)
     if tag:
-        match = compiled_pattern.search(tag.string)
+        match = pattern.search(tag.string)
         return tag, match
     return None, None
