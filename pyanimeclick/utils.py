5bb0db1f047831f913edb61dd319dee78130145d
stefanodvx <69367859+stefanodvx@users.noreply.github.com>
2023-05-21 12:53:03 +0200

    Update utils.py

--- pyanimeclick/utils.py
+++ pyanimeclick/utils.py
@@ -6,6 +6,8 @@ BASE_URL = "https://www.animeclick.it"
 TITLE_PAGE = BASE_URL + "/anime/{}/_"
 SEARCH_PAGE = BASE_URL + "/cerca"
 
+COVER_PATTERN = r"(?P<name>\/.+)(?P<ext>\.\w+)$"
+
 def find_matchin_tag(
     soup: BeautifulSoup,
     name: str,
@@ -20,3 +22,14 @@ def find_matchin_tag(
 
 def resolve_path(path: str):
     return BASE_URL + path
+
+def get_cover(path: str):
+    match = re.search(COVER_PATTERN, path)
+    if not match:
+        return None
+    name, ext = match.groups()
+    for suffix in ("-thumb-mini", "-thumb", "-mini"):
+        abs_name = name.rstrip(suffix)
+    hd_cover = resolve_path(abs_name + ext)
+    original_cover = resolve_path(path)
+    return hd_cover, original_cover
