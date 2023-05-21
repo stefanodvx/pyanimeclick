25c05f245452f258d074074ca7db4b63e9e1aae3
stefanodvx <69367859+stefanodvx@users.noreply.github.com>
2023-05-21 12:01:07 +0200

    Update utils.py

--- pyanimeclick/utils.py
+++ pyanimeclick/utils.py
@@ -2,11 +2,15 @@ from bs4 import BeautifulSoup
 
 import re
 
-def find_tag(soup: BeautifulSoup, name: str, pattern: str, **kwargs):
+def find_matchin_tag(
+    soup: BeautifulSoup,
+    name: str,
+    pattern: str,
+    **kwargs
+):
     compiled_pattern = re.compile(pattern)
     tag = soup.find(name, **kwargs, string=compiled_pattern)
     if tag:
         match = compiled_pattern.search(tag.string)
-        if not match:
-            return tag
         return tag, match
+    return None, None
