fba15c600f93cfe2e104d6325eafce3b5a2f30c4
stefanodvx <69367859+stefanodvx@users.noreply.github.com>
2023-05-22 23:44:33 +0200

    Update get_title.py

---
+++ pyanimeclick/methods/get_title.py
@@ -0,0 +1,23 @@
+from ..types import Title
+
+from ..utils import TITLE_PAGE
+
+from typing import Optional
+from bs4 import BeautifulSoup
+
+import pyanimeclick
+import logging
+
+log = logging.getLogger(__name__)
+
+class GetTitle:
+    async def get_title(
+        self: "pyanimeclick.AnimeClick",
+        id: int
+    ) -> Optional["Title"]:
+        response = await self._make_request(
+            method="GET",
+            url=TITLE_PAGE.format(str(id)),
+        )
+        soup = BeautifulSoup(response.text, "lxml")
+        return Title._parse(soup)
